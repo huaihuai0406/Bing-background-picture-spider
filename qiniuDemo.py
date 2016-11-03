da8043daa6d29bf48003df2fa952ac65afb55ab4
silenceFei <feistudy@163.com>
2016-11-03 23:14:04 +0800

    add qiniu upload

---
+++ qiniuDemo.py
@@ -0,0 +1,19 @@
+from qiniu import Auth, put_file, etag, urlsafe_base64_encode
+import qiniu.config
+import os
+
+assess_key = os.environ.get('QINIUAk')
+secret_key = os.environ.get('QINIUSK')
+
+q = Auth(assess_key, secret_key)
+
+bucket_name = 'bing-background-picture'
+
+key = 'temp.jpg'
+token = q.upload_token(bucket_name, key, 3600)
+
+localfile = 'E:\\temp.jpg'
+ret, info = put_file(token, key, localfile)
+print ret, info
+assert ret['key'] == key
+assert ret['hash'] == etag(localfile)
